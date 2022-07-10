b01c8f999ff3782a9861487ee7fddcea97cbb6a5
alejandropl07 <aprietoleon97@gmail.com>
2022-07-09 22:28:47 -0500

    Config redux

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
-import './App.css';
+import "./App.css";
+import { useSelector } from "react-redux";
 
 function App() {
+  const { tasks } = useSelector((state) => state);
+  console.log(tasks);
   return (
     <div className="App">
       <h1>CRUD Redux Toolkit</h1>
