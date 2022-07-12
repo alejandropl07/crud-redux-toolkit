e8140ba4c7a7e3f7a296be9aeaa86f2dd71872c4
alejandropl07 <aprietoleon97@gmail.com>
2022-07-11 19:24:25 -0500

    Styles with Tailwind

--- src/components/TaskForm.js
+++ src/components/TaskForm.js
@@ -48,22 +48,32 @@ const TaskForm = () => {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
+    <form onSubmit={handleSubmit} className="bg-zinc-800 max-w-sm p-4">
+      <label htmlFor="title" className="block text-xs font-bold mb-2">
+        Task:
+      </label>
       <input
         type="text"
         name="title"
         placeholder="Title"
         value={task.title}
         onChange={handleChange}
+        className="w-full p-2 rounded-md bg-zinc-600 mb-2"
       />
+      <label htmlFor="description" className="block text-xs font-bold mb-2">
+        Description:
+      </label>
       <textarea
         name="description"
         placeholder="Description"
         value={task.description}
         onChange={handleChange}
+        className="w-full p-2 rounded-md bg-zinc-600 mb-2"
       ></textarea>
 
-      <button type="submit">Save</button>
+      <button type="submit" className="bg-indigo-600 px-2 py-1">
+        Save
+      </button>
     </form>
   );
 };
